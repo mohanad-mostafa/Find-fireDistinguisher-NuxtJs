5f8bc4a774910c6a42c4db2464e0e43629bfbdc0
Mohanad <mohannad.mostafa98@gmail.com>
2022-07-26 10:00:39 +0200

    Created project

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <HeroSection />
+  </div>
+</template>
+
+<script>
+import HeroSection from '../components/HeroSection.vue'
+export default {
+  name: 'IndexPage',
+  components: { HeroSection }
+}
+</script>
